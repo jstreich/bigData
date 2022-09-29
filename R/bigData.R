8c0b719b9286494deb3bfdbb4067cfc0d43e58a1
Streich <ju0@mac128036.ornl.gov>
2022-09-28 21:07:40 -0400

    first commit

---
+++ R/bigData.R
@@ -0,0 +1,11 @@
+#' bigData Package
+#'
+#' Contains functions to pre-process large data files based on conditions
+#' Also merging data sets before, after, or during data loading into memory.
+#' Lastly, it has some nice parsing functions once dat is loaded.
+#' @docType
+#'
+#' @author Jared Streich \email{streich.jared@gmail.com}
+#'
+#' @name bigData
+NULL
