8fbd92472a36dc0dd5b49126a433844b760ecdf7
Jared Streich <streich.jared@gmail.com>
2023-03-02 19:56:45 -0500

    add params and export

--- R/bd_colRange.R
+++ R/bd_colRange.R
@@ -1,5 +1,14 @@
 #' Get range of columns
 #' Description: Like bd_subsetRow, this function will get a random subset of columns to assess data structure and properties. Ideal if samples are encoded column wise.
+#' @param x Dataframe X
+#' @param start Dataframe Start Column
+#' @param end Dataframe End Column
+#' @param colnms column names present T/F
+#' @param sep sep = "" set delimiter
+#' @param rownms rowumn names present T/F
+#' 
+#' @reads in a file's specific column range specified by the user.
+#' @export
 bd_colRange <- function(x, start, end, colnms, sep, rownms){
   system(paste("cut -d'", sep, "' -f", start, "-", end, " ", x, "> bd_colRange.jcs", sep = ""))
   output <- read.delim("bd_colRange.jcs", header = colnms, sep = sep)
