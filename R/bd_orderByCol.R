c69adc135c1c606470e6705f30da026b485e89e3
Jared Streich <streich.jared@gmail.com>
2023-03-02 20:56:59 -0500

    added params and export

--- R/bd_orderByCol.R
+++ R/bd_orderByCol.R
@@ -1,5 +1,12 @@
 #' Order x rows by y rows from two specific columns
 #' Description: Like bd_2MatrixColMatch, this funciton takes two matrices, x and y, but orders an x-specific column by a y-specific column.
+#' @param x data frame to order upon
+#' @param xc column of x to order upon
+#' @param y data frame to use for ordering
+#' @param yc column of y to use for ordering
+#'
+#' @return returns the minimum, maximum, mean, standard deviation, normality, quartiles of each column in a file
+#' @export
 bd_orderByCol <- function(x, xc, y, yc){
   x.o <- match(x[,xc], y[,yc])
   x.s <- x[order(x.o), ]
