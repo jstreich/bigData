769555b2fada6bb7f45f6a6610ea59d434d0d1c4
Jared Streich <streich.jared@gmail.com>
2023-03-02 21:00:39 -0500

    added params and export

--- R/bd_orderColNames.R
+++ R/bd_orderColNames.R
@@ -1,5 +1,10 @@
 #' Order by colnames
 #' Description: Order maxtrix x by matrix y's columns
+#' @param x data frame X
+#' @param y data frame Y
+#'
+#' @return data frame x where shared columns with y are ordered by data frame y
+#' @export
 bd_orderColNames <- function(x,y){
   x.t <- t(x)
   y.t <- t(y)
