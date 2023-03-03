a362de2b43e1cd97209e5623302f3dcca341c460
Jared Streich <streich.jared@gmail.com>
2023-03-02 22:21:00 -0500

    Update bd_orderRowNames.R

--- R/bd_orderRowNames.R
+++ R/bd_orderRowNames.R
@@ -1,9 +1,11 @@
 #' Order by Rownames
+#'
 #' Description: Order matrix rownames by matrix y's rowname order
 #' @param x data frame to order rownames on
 #' @param y data frame to order x with
 #'
 #' @return returns data frame x after ordering shared rows with data frame y
+#'
 #' @export
 bd_orderRowNames <- function(x,y){
   x.o <- match(rownames(x), rownames(y))
