95c4196e7f140b79aeea33c17d990ff339926794
Jared Streich <streich.jared@gmail.com>
2023-03-02 20:27:41 -0500

    Update bd_dim.R
    add param and export

--- R/bd_dim.R
+++ R/bd_dim.R
@@ -1,5 +1,9 @@
 #' Get Dimensions of file
 #' Description: This function returns the number of rows and columns in a data set without actually loading the file. This helps the user decide if all or part of a file should be loaded.
+#' @param x calculates the dimensions of a file without reading it into memory
+#'
+#' @return prints the dimensions of a file yet to be loaded
+#' @export
 bd_dim <- function(x){
   system(paste("awk '{print NF}'", x, "| sort -nu | tail -n 1 > cls.jcs"))
   system(paste("less", x, "| wc -l > rws.jcs"))
