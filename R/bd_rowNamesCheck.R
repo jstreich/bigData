17b705c411e374bc7252ada16d8ca4418de3ae52
Jared Streich <streich.jared@gmail.com>
2023-03-02 21:20:41 -0500

    add export and params

--- R/bd_rowNamesCheck.R
+++ R/bd_rowNamesCheck.R
@@ -1,5 +1,10 @@
 #' Rownames Check'
 #' Description: Creates a simple plot of dots representing the x and y positions of sample IDs, a 0.5 slope indicates perfect matching, points not on a 0.5 slope indicate sample IDs that do not correspond between data sets.'
+#' @param x data frame check rowname order with
+#' @param y data frame check rowname order with
+#'
+#' @return returns data frame x after ordering shared rows with data frame y
+#' @export
 bd_rowNamesCheck <- function(x, y){
   plot(match(rownames(x), rownames(y)))
 }
