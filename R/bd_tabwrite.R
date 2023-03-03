bda6e9b74204fc0dd60a82745a1b4aca65794f89
Jared Streich <streich.jared@gmail.com>
2023-03-02 21:30:48 -0500

    add params and export

--- R/bd_tabwrite.R
+++ R/bd_tabwrite.R
@@ -1,5 +1,13 @@
 #' Simplified write file function
 #' Description: This function will write a table of data to file using tabs delimitation without quotes
+#' @param x data frame to output to file
+#' @param y data frame check rowname order with
+#' @param name = "[your_desired_outut_name.ext]"
+#' @param rownms output rownames T/F TRUE/FALSE
+#' @param colnms output colnames T/F TRUE/FALSE
+#'
+#' @return returns data frame x after ordering shared rows with data frame y
+#' @export
 bd_tabwrite <- function(x, name, rownms, colnms){
   write.table(x, nm, row.names = rn, col.names = cn, quote = F, sep = "  ")
 }
