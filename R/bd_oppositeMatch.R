981d0c21f7e5545f2ec67a3d7d4db7f53cb35984
Jared Streich <streich.jared@gmail.com>
2023-03-02 20:49:22 -0500

    add params and export

--- R/bd_oppositeMatch.R
+++ R/bd_oppositeMatch.R
@@ -1,5 +1,10 @@
 #' Get samples in x not in y
 #' Description: This function returns non-matching sample's of x that may be in y. This is expecially useful for making training/prediction data so desparate samples are used for prediction testing.
+#' @param x dataframe to filter anti-union of y
+#' @param y data frame to filter x rownames upon
+#'
+#' @return Returns samples of x not found in y
+#' @export
 bd_oppositeMatch <- function(x, y){
   n.m <- !match(rownames(x), rownames(y))
   n.b <- cbind(n.m, x)
