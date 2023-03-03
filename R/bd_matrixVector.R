cbcad1fc2919ecb0b87706bd703acc835d1b9440
Jared Streich <streich.jared@gmail.com>
2023-03-02 22:17:33 -0500

    Update bd_matrixVector.R

--- R/bd_matrixVector.R
+++ R/bd_matrixVector.R
@@ -3,7 +3,7 @@
 #' Description: This function returns x as vector encoded strings row-wise
 #' @param x converts a variable 2D matrix/array into vector encoding values.
 #'
-#' @return a 2D array as an all vector data set.
+#' @returns a 2D array as an all vector data set.
 #'
 #' @export
 bd_matrixVector <- function(x){
