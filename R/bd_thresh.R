0a99f1ffde208982744878258bbb08f386299b31
Jared Streich <streich.jared@gmail.com>
2023-03-02 22:28:28 -0500

    Update bd_thresh.R

--- R/bd_thresh.R
+++ R/bd_thresh.R
@@ -1,5 +1,6 @@
 #' Read in values smaller, smaller-equal, exactly, greater-equal, greater than in specific column
 #' Description: This function reads in a file, but only loads specific rows of data based on minimum, exact, or maximum thresholds. This is useful if a file has many rows, but a specific column of importance, like if column seven has p-values you can only load rows where column seven is less than 0.2. Or other data types where specific categorical values, names must match, Or greater than some specific threshold, like temperature minimums in a list of measurements.
+#'
 #' @param x A/file/to/read/in/from/a/directory/filename.txt
 #' @param colnms if you want to read in column names colnms = T/F or TRUE/FALSE
 #' @param rownms if you want to read in row names colnms = T/F or TRUE/FALSE
@@ -7,7 +8,8 @@
 #' @param sep Set for delimter of the file you're reading in from, sep = "[delimiter]"
 #' @param method The type of thresholding filter method: "smaller" = max value, "smaller-equal" = min equal value, "greater-equal" = max or equal value, "greater" = minimum value, "exactly" = exact values (ideal for loading categorical constraints), 
 #'
-#' @return a filtered version of a file based on specified column and column filtering conditions during data loading of a large file.
+#' @returns A filtered version of a file based on specified column and column filtering conditions during data loading of a large file.
+#'
 #' @export
 bd_thresh <- function(x, column, colnms, rownms, threshold, sep, method){
   if(method == "smaller"){
