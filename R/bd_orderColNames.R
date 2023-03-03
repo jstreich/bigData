8dd7a36fd46d77c3ca2e9e4d63a480c8ed22c444
Jared Streich <streich.jared@gmail.com>
2023-03-02 22:19:32 -0500

    Update bd_orderColNames.R

--- R/bd_orderColNames.R
+++ R/bd_orderColNames.R
@@ -3,7 +3,7 @@
 #' @param x data frame X
 #' @param y data frame Y
 #'
-#' @return data frame x where shared columns with y are ordered by data frame y
+#' @returns Data frame x where shared columns with y are ordered by data frame y
 #' @export
 bd_orderColNames <- function(x,y){
   x.t <- t(x)
